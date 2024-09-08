6f08a4ba4fc6f4be0bc2feba342a02362812d96b
arslanhakim <arslanhaakim@gmail.com>
2024-09-07 20:25:00 -0400

    Header hamberger updated

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -157,26 +157,26 @@ const Header: React.FC<HeaderProps> = ({
               <MdKeyboardArrowRight className="inline-block self-center" />
             </span>
           </button>
-          <button
-            className="md:hidden bg-primary outline-none border-0"
-            onClick={handleMenuToggle}
-          >
-            <svg
-              className="w-6 h-6 text-white"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d={isOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16m-7 6h7"}
-              />
-            </svg>
-          </button>
         </nav>
+        <button
+          className="md:hidden bg-primary outline-none border-0"
+          onClick={handleMenuToggle}
+        >
+          <svg
+            className="w-6 h-6 text-white"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d={isOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16m-7 6h7"}
+            />
+          </svg>
+        </button>
       </div>
       <nav
         className={menuClasses}
