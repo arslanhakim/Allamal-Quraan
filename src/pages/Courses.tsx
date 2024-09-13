fb8f784264f24acbc69122dad7bac2967a75925d
arslanhakim <arslanhaakim@gmail.com>
2024-09-13 10:50:20 -0400

    courses page

--- src/pages/Courses.tsx
+++ src/pages/Courses.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useRef } from "react";
+import Header from "../components/Header";
 
 const Courses = () => {
-  return <div>Courses</div>;
+  const contactRef = useRef(null);
+  const aboutUsRef = useRef(null);
+  const coursesRef = useRef(null);
+  const instructorsRef = useRef(null);
+  const faqRef = useRef(null);
+  return (
+    <>
+      <div className="min-w-screen h-screen w-full maxWidth-full lg:px-6 pb-12 lg:py-12 bg-gray-extra text-black-dark text-center top-0">
+        <Header
+          contactRef={contactRef}
+          aboutUsRef={aboutUsRef}
+          coursesRef={coursesRef}
+          instructorsRef={instructorsRef}
+          faqRef={faqRef}
+        />
+      </div>
+    </>
+  );
 };
 
 export default Courses;
